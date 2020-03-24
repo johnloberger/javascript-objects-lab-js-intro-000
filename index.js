4effcbad9932313dcc27c8ce032ee83d56018e61
John Loberger <johnloberger@gmail.com>
2020-03-24 18:34:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var recipes = {'object':'key'}
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object 
+}
